bf001063f298f8966ed725980451f23b7172e932
allxie <allxiecleary@gmail.com>
2022-09-14 18:08:02 -0300

    MVP for portuguese conjugation app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,207 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import sentences from './sentences';
 import './App.css';
 
+const subjects = [
+  {key: 'eu', display: 'Eu', english: 'I'},
+  {key: 'voce', display: 'Você', english: 'You'},
+  {key: 'elas', display: 'Elas', english: 'They'}
+]
+
+const tenses = [
+  {key: 'present', display: 'present'},
+  {key: 'pastPerfect', display: 'past perfect'},
+  {key: 'pastImperfect', display: 'past imperfect'}
+]
+
+const getRandom = (array) => {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 function App() {
+  const [goToNext, setGoToNext] = useState(0)
+  const [inputValue, setInputValue] = useState('')
+
+  const [availableVerbs, setAvailableVerbs] = useState([0])
+
+  const [sentence, setSentence] = useState(sentences[getRandom(availableVerbs)])
+  const [subject, setSubject] = useState(getRandom(subjects))
+  const [tense, setTense] = useState(getRandom(tenses))
+
+  const [success, setSuccess] = useState(false)
+
+  const [hintCount, setHintCount] = useState(0)
+
+  const reset = () => {
+    setSentence(sentences[getRandom(availableVerbs)])
+    setTense(getRandom(tenses))
+    setSubject(getRandom(subjects));
+    setSuccess(false);
+    setInputValue('');
+    setGoToNext(0)
+    setHintCount(0)
+  }
+
+  const hint = () => {
+    setHintCount(hintCount + 1)
+  }
+
+  const answer = sentence.verbConjugation[tense.key][subject.key]
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if(success) return reset()
+      if(goToNext === 1) return reset()
+
+      setGoToNext(1)
+    }
+  }
+
+  /* Checklist */
+
+  const handleVerbCheck = (e) => {
+    const index = Number(e.target.value)
+
+    var updatedList = [...availableVerbs]
+    if (e.target.checked) {
+      updatedList = [...availableVerbs, index]
+    } else {
+      updatedList.splice(availableVerbs.indexOf(index), 1)
+    }
+
+    setAvailableVerbs(updatedList);
+  }
+
+  const isChecked = (index) => {
+    return availableVerbs.indexOf(index) !== -1
+  }
+
+  const getEnglishSentenceColor = () => {
+    if(hintCount > 1) return 'grey'
+    if(success) return 'green'
+    else return '#282c34'
+  }
+
+  /* Main Render */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="App-header">   
+        <p style={{
+          fontSize: '1em',
+          color: getEnglishSentenceColor(),
+        }}>
+          {`${subject.english} ${sentence.englishVerbConjugation[tense.key]} ${sentence.secondHalfEnglish}`}
+        </p>        
+        <div
+          style={{flexDirection:"row", height: '55px'}}
         >
-          Learn React
-        </a>
+          {subject.display} 
+          <input
+            style={{
+              padding: '10px',
+              margin: '10px',
+              border: '0px',
+              display: hintCount > 1 ? 'none' : 'inline',
+              backgroundColor: success ? 'green' : 'white'
+            }}
+            placeholder={sentence.englishVerbConjugation[tense.key]}
+            value={inputValue}
+            onChange={(e)=> {
+              setInputValue(e.target.value)
+              setGoToNext(0)
+              if(e.target.value === answer) {
+                setSuccess(true)
+              }
+            }}
+            onKeyDown={handleKeyDown}
+          />
+          <span
+            style={{
+              display: hintCount < 2 ? 'none' : 'inline',
+            }}
+          >
+            {` ${answer} `}
+          </span>
+          {sentence.secondHalf}
+        </div>
+
+        <div style={{
+          fontSize: '.7em',
+          display: success ? 'none' : 'block',
+          color: 'grey',
+          height: '40px'
+        }}>
+          {`${hintCount < 1 ? sentence.verbEnglish : sentence.verbRoot} (${tense.display})`}  
+        </div>
+        
+        <div style={{
+          display: success ? 'block': 'none',
+          color: 'grey',
+          height: '40px'
+        }}>
+          Press "enter" to continue
+        </div>
+
+        <div style={{
+          display: goToNext ? 'block': 'none',
+          color: 'grey'
+        }}>
+          Press "enter" again to skip
+        </div>
+
+        {/* BUTTONS */}
+
+        <div>
+          <button 
+            style={{marginTop: "20px", marginRight: '15px'}}
+            onClick={() => hint()}
+          >
+            Hint
+          </button>
+          <button 
+            style={{marginTop: "20px"}}
+            onClick={() => reset()}
+          >
+            Next
+          </button>
+        </div>
+
+        {/* CHECKBOX */}
+        <div
+          style={{
+            position: 'fixed',
+            top: '10px',
+            right: '10px',
+            padding: '20px',
+            border: '1px solid white',
+            width: '20vw',
+            fontSize:'.7em',
+            justifyContent: 'left',
+            textAlign: 'left'
+          }}
+        >
+        {
+          sentences.map((sentence, index) => {
+            return (
+              <div key={index}>
+                <input 
+                  value={index} 
+                  type="checkbox" 
+                  onChange={handleVerbCheck}
+                  defaultChecked={isChecked(index)}
+                />
+                <span 
+                  style={{
+                    color: isChecked(index) ? 'white' : 'grey'
+                  }}
+                >
+                  {sentence.verbEnglish}  
+                </span>
+              </div>
+            )
+          })
+        }
+        </div>
       </header>
     </div>
   );
---
+++ src/sentences.js
@@ -0,0 +1,280 @@
+module.exports = [
+  {
+    verbRoot: "ir",
+    verbEnglish: "to go",
+    verbConjugation: {
+      present: {
+        eu: 'vou',
+        voce: 'vai',
+        elas: 'vão'
+      },
+      pastPerfect: {
+        eu: 'fui',
+        voce: 'foi',
+        elas: 'foram'
+      },
+      pastImperfect: {
+        eu: 'ia',
+        voce: 'ia',
+        elas: 'iam'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'go',
+      pastPerfect: 'went',
+      pastImperfect: 'used to go',
+    },
+    secondHalf: "a aula.",
+    secondHalfEnglish: "to class."
+  },
+  {
+    verbRoot: "ter",
+    verbEnglish: "to have",
+    verbConjugation: {
+      present: {
+        eu: 'tenho',
+        voce: 'ten',
+        elas: 'têm'
+      },
+      pastPerfect: {
+        eu: 'tive',
+        voce: 'teve',
+        elas: 'tiveram'
+      },
+      pastImperfect: {
+        eu: 'tinha',
+        voce: 'tinha',
+        elas: 'tinham'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'have',
+      pastPerfect: 'had',
+      pastImperfect: 'used to have'
+    },
+    secondHalf: "um lápis.",
+    secondHalfEnglish: 'a pencil.'
+  },
+  {
+    verbRoot: "estar",
+    verbEnglish: "to be (state)",
+    verbConjugation: {
+      present: {
+        eu: 'estou',
+        voce: 'está',
+        elas: 'estão'
+      },
+      pastPerfect: {
+        eu: 'estive',
+        voce: 'esteve',
+        elas: 'estiveram'
+      },
+      pastImperfect: {
+        eu: 'estava',
+        voce: 'estava',
+        elas: 'estavam'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'are',
+      pastPerfect: 'were',
+      pastImperfect: 'used to be'
+    },
+    secondHalf: "no Brasil.",
+    secondHalfEnglish: 'in Brazil.'
+  },
+  {
+    verbRoot: "ser",
+    verbEnglish: "to be (trait)",
+    verbConjugation: {
+      present: {
+        eu: 'seu',
+        voce: 'é',
+        elas: 'sæo'
+      },
+      pastPerfect: {
+        eu: 'fui',
+        voce: 'foi',
+        elas: 'foram'
+      },
+      pastImperfect: {
+        eu: 'era',
+        voce: 'era',
+        elas: 'eram'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'are',
+      pastPerfect: 'were',
+      pastImperfect: 'used to be'
+    },
+    secondHalfEnglish: 'intelligent.',
+    secondHalf: "inteligentes."
+  },
+  {
+    verbRoot: "trazer",
+    verbEnglish: "to bring",
+    verbConjugation: {
+      present: {
+        eu: 'trago',
+        voce: 'traz',
+        elas: 'trazem'
+      },
+      pastPerfect: {
+        eu: 'trouxe',
+        voce: 'trouxe',
+        elas: 'trouxeram'
+      },
+      pastImperfect: {
+        eu: 'trazia',
+        voce: 'trazia',
+        elas: 'traziam'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'bring',
+      pastPerfect: 'brought',
+      pastImperfect: 'used to bring'
+    },
+    secondHalfEnglish: 'a pencil to class.',
+    secondHalf: "um lápis a aula."
+  },
+  {
+    verbRoot: "fazer",
+    verbEnglish: "to do/make",
+    verbConjugation: {
+      present: {
+        eu: 'faco',
+        voce: 'faz',
+        elas: 'fazem'
+      },
+      pastPerfect: {
+        eu: 'fiz',
+        voce: 'fez',
+        elas: 'fizeram'
+      },
+      pastImperfect: {
+        eu: 'fazia',
+        voce: 'fazia',
+        elas: 'faziam'
+      }
+    },
+    englishVerbConjugation: {
+      present: 'do',
+      pastPerfect: 'did',
+      pastImperfect: 'used to do'
+    },
+    secondHalfEnglish: 'some homework.',
+    secondHalf: "alguns lição de casa."
+  },
+  // {
+  //   verbRoot: "saber",
+  //   verbEnglish: "to know",
+  //   verbConjugation: {
+  //     present: {
+  //       eu: 'sei',
+  //       voce: 'sabe',
+  //       elas: 'sabem'
+  //     },
+  //     pastPerfect: {
+  //       eu: 'soube',
+  //       voce: 'soube',
+  //       elas: 'souberam'
+  //     },
+  //     pastImperfect: {
+  //       eu: 'sabia',
+  //       voce: 'sabia',
+  //       elas: 'sabiam'
+  //     }
+  //   },
+  //   secondHalf: "falar Portuguese."
+  // },
+  // {
+  //   verbRoot: "querer",
+  //   verbEnglish: "to want",
+  //   verbConjugation: {
+  //     present: {
+  //       eu: 'quero',
+  //       voce: 'quer',
+  //       elas: 'querem'
+  //     },
+  //     pastPerfect: {
+  //       eu: 'quis',
+  //       voce: 'quis',
+  //       elas: 'quiseram'
+  //     },
+  //     pastImperfect: {
+  //       eu: 'queria',
+  //       voce: 'queria',
+  //       elas: 'queriam'
+  //     }
+  //   },
+  //   secondHalf: "falar Portuguese."
+  // },
+  // {
+  //   verbRoot: "vir",
+  //   verbEnglish: "to come",
+  //   verbConjugation: {
+  //     present: {
+  //       eu: 'venho',
+  //       voce: 'vem',
+  //       elas: 'vêm'
+  //     },
+  //     pastPerfect: {
+  //       eu: 'vim',
+  //       voce: 'veio',
+  //       elas: 'vieram'
+  //     },
+  //     pastImperfect: {
+  //       eu: 'vinha',
+  //       voce: 'vinha',
+  //       elas: 'vinham'
+  //     }
+  //   },
+  //   secondHalf: "para o Brasil."
+  // },
+  // {
+  //   verbRoot: "poder",
+  //   verbEnglish: "to be able to",
+  //   verbConjugation: {
+  //     present: {
+  //       eu: 'posso',
+  //       voce: 'pode',
+  //       elas: 'podem'
+  //     },
+  //     pastPerfect: {
+  //       eu: 'pude',
+  //       voce: 'pôde',
+  //       elas: 'puderam'
+  //     },
+  //     pastImperfect: {
+  //       eu: 'podia',
+  //       voce: 'podia',
+  //       elas: 'podiam'
+  //     }
+  //   },
+  //   secondHalf: "falar Portuguese."
+  // },
+  // {
+  //   verbRoot: "dizer",
+  //   verbEnglish: "to say",
+  //   verbConjugation: {
+  //     present: {
+  //       eu: 'digo',
+  //       voce: 'diz',
+  //       elas: 'dizem'
+  //     },
+  //     pastPerfect: {
+  //       eu: 'disse',
+  //       voce: 'disse',
+  //       elas: 'disseran'
+  //     },
+  //     pastImperfect: {
+  //       eu: 'dizia',
+  //       voce: 'dizia',
+  //       elas: 'diziam'
+  //     }
+  //   },
+  //   secondHalf: "sim."
+  // },
+]
